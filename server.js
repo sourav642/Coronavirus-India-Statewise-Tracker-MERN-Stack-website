588afb3f9be1733509bc8d23478cfdcf74c74694
Sourav Acharya Chowdhury <sac642@gmail.com>
2020-05-17 20:17:56 +0530

    Final Website

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors')
+//const bodyParser = require('body-parser');
+//const cors = require('cors')
 const APIRouter = require('./routes/API');
 var path = require('path');
-require('dotenv').config();
+//require('dotenv').config();
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('./client/build'));
 
